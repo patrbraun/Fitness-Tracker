d00af65fab7cef623bd7b03b1fea11444d246eeb
Patrick Braun <prbraun@protonmail.com>
2021-06-28 22:24:26 -0500

    creating the workout model

--- controllers/html/html-routes.js
+++ controllers/html/html-routes.js
@@ -1,16 +1,36 @@
 const router = require('express').Router();
 
-// GET all galleries for homepage
+// GET homepage
 router.get("/", async (req, res) => {
   console.log("GET /");
   try {
-    res.redirect("/html/index.html");
+    res.redirect("/index.html");
   } catch (err) {
     console.error(err);
     res.status(401).json(err);
   }
 });
 
+// GET exercise page
+router.get("/exercise", async (req, res) => {
+  console.log("GET /exercise");
+  try {
+    res.redirect("/exercise.html");
+  } catch (err) {
+    console.error(err);
+    res.status(401).json(err);
+  }
+});
 
+// GET stats page
+router.get("/stats", async (req, res) => {
+  console.log("GET /stats");
+  try {
+    res.redirect("/stats.html");
+  } catch (err) {
+    console.error(err);
+    res.status(401).json(err);
+  }
+});
 
 module.exports = router;
